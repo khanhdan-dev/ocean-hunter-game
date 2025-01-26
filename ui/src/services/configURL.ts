3b37f9480d725dd3a9cdc67cde6154cb65ecb759
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.com>
2025-01-26 09:22:49 +0700

    adjust BASE_URL production

--- ui/src/services/configURL.ts
+++ ui/src/services/configURL.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = process.env.SERVER_PORT
   ? `http://localhost:${process.env.SERVER_PORT}/`
-  : 'https://ocean-tab-game-api.vercel.app/';
+  : 'https://ocean-hunter-game.vercel.app/api';
 
 export const https = axios.create({
   baseURL: BASE_URL,

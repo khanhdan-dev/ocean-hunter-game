81d67ac863b8361a2e95f0beae2b2c73104cb0a6
Khanh Dan Nguyen Nhat <dan.nhatkhanh@gmail.com>
2025-01-27 08:58:38 +0700

    keep optimizing api vercel

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,20 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-
-  // app.enableCors();
-  const port = process.env.PORT || 5000;
-  await app.listen(port);
+  app.useGlobalPipes(new ValidationPipe()); //This line was moved to ensure consistent order.
+  await app.listen(process.env.PORT || 5000);
 }
+
 bootstrap();
 
-// Export the Express app for Vercel
+// Serverless function for Vercel
 export const handler = async (req, res) => {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe()); //This line was moved to ensure consistent order.
   await app.init();
-  const expressApp = app.getHttpAdapter().getInstance();
-  return expressApp(req, res);
+  const instance = app.getHttpAdapter().getInstance();
+  return instance(req, res);
 };
